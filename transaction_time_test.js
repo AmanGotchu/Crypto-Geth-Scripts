26884980d717f1f29af781d13722b7b95b829b4c
Aman Gottumukkala <aman.gottu@gmail.com>
2021-04-14 00:34:03 -0500

    Update transaction_time_test.js

--- transaction_time_test.js
+++ transaction_time_test.js
@@ -1,3 +1,41 @@
+function waitForBlocks(){
+    if(eth.getTransaction(transactions[transactions.length-1].trans).blockNumber != null){
+        blocks = []
+
+        time_sum = 0
+        for(var j = 0; j<transactions.length; j++) {
+            blockNumber = eth.getTransaction(transactions[j].trans).blockNumber
+            block = eth.getBlock(blockNumber)
+            block.blockNumber = blockNumber
+            if(blocks.length == 0 || blocks[blocks.length-1].blockNumber != blockNumber) {
+                blocks.push(block)
+            }
+
+            block_mined_time = block.timestamp
+            start_time = transactions[j].timestamp/1000
+
+            time_sum += block_mined_time - start_time
+        }
+
+        average_transaction_mine_time = time_sum / transactions.length
+        console.log("Average transaction mine time: ", average_transaction_mine_time)
+        for(var z = 0; z<blocks.length; z++) {
+            // Number of transactions in block
+            console.log("Block Number: ", blocks[z].blockNumber)
+            console.log("Number of transactions: ", blocks[z].transactions.length)
+            console.log("Gas used ", blocks[z].gasUsed)
+            console.log("Block size (bytes): ", blocks[z].size)
+            console.log("Total Difficulty: ", blocks[z].totalDifficulty)
+            console.log("\n")
+        }
+
+        miner.stop()
+    }
+    else{
+        setTimeout(waitForBlocks, 250);
+    }
+}
+
 transactions = []
 
 amt = 0
@@ -12,37 +50,5 @@ while(amt < 100000) {
 
 miner.start()
 
-while(eth.getTransaction(transactions[transactions.length-1].trans).blockNumber == null) {
-    sleep(2000);
-}
+waitForBlocks()
 
-blocks = []
-
-time_sum = 0
-for(var j = 0; j<transactions.length; j++) {
-    blockNumber = eth.getTransaction(transactions[j].trans).blockNumber
-    block = eth.getBlock(blockNumber)
-    block.blockNumber = blockNumber
-    if(blocks.length == 0 || blocks[blocks.length-1].blockNumber != blockNumber) {
-        blocks.push(block)
-    }
-
-    block_mined_time = block.timestamp
-    start_time = transactions[j].timestamp/1000
-
-    time_sum += block_mined_time - start_time
-}
-
-average_transaction_mine_time = time_sum / transactions.length
-console.log("Average transaction mine time: ", average_transaction_mine_time)
-for(var z = 0; z<blocks.length; z++) {
-    // Number of transactions in block
-    console.log("Block Number: ", blocks[z].blockNumber)
-    console.log("Number of transactions: ", blocks[z].transactions.length)
-    console.log("Gas used ", blocks[z].gasUsed)
-    console.log("Block size (bytes): ", blocks[z].size)
-    console.log("Total Difficulty: ", blocks[z].totalDifficulty)
-    console.log("\n")
-}
-
-miner.stop()
